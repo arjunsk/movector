de03bb0f139e8a21619c1ee11621f8cd850d824c
arjunsk <arjunsk@users.noreply.github.com>
2023-08-20 23:19:01 -0700

    Added support for pgvector.

--- movector/sqlalchemy/__init__.py
+++ movector/sqlalchemy/__init__.py
@@ -14,9 +14,9 @@ class VectorF32(UserDefinedType):
     def get_col_spec(self, **kw):
         if self.dim is None:
             # return "VECTOR"
-            return "ARRAY_FLOAT32"
+            return "VECF32"
         # return "VECTOR(%d)" % self.dim
-        return "ARRAY_FLOAT32(%d)" % self.dim
+        return "VECF32(%d)" % self.dim
 
     def bind_processor(self, dialect):
         def process(value):
