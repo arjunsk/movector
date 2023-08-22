35ac0f8861a433d6b93a657ae0b3353557dd2957
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-08-22 15:54:15 -0700

    Added asyncpg

--- pgvector/asyncpg/__init__.py
+++ pgvector/asyncpg/__init__.py
@@ -1,11 +1,11 @@
-from ..utils import from_db, from_db_binary, to_db, to_db_binary
+from pgvector.utils import from_db_binary, to_db_binary
 
 __all__ = ['register_vector']
 
 
 async def register_vector(conn):
     await conn.set_type_codec(
-        'VECF32',
+        'vector',
         encoder=to_db_binary,
         decoder=from_db_binary,
         format='binary'
