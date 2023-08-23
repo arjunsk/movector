013d469b1d9150b4eea5bd2f1fb6f495b16d584b
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-08-23 15:09:37 -0700

    comment print

--- movector/utils/__init__.py
+++ movector/utils/__init__.py
@@ -44,5 +44,6 @@ def to_db_binary(value, dim=None):
     if value.ndim != 1:
         raise ValueError('expected ndim to be 1')
 
+    # print(value.tobytes())
     return value.tobytes()
     # return binascii.b2a_hex(value)
